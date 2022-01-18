62b5eeebe1c278f6b955d29362fc51b79b31c758
PalasanRares <rarespalasan@outlook.com>
2022-01-18 03:36:20 +0200

    initial commit

---
+++ server/main_process.py
@@ -0,0 +1,40 @@
+import socket
+import json
+from threading import Thread
+
+
+class ClientThread(Thread):
+
+    def __init__(self, ip, port, connection):
+        Thread.__init__(self)
+        self.ip = ip
+        self.port = port
+        self.connection = connection
+        print("New thread created for ip " + ip + " port " + str(port))
+    
+
+def read_from_file(file_name):
+    ip_and_port = json.load(open(file_name))
+    return ip_and_port["localIp"], ip_and_port["localPort"]
+
+
+def run():
+    local_ip, local_port = read_from_file("server_address.json")
+
+    tcp_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
+    tcp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    tcp_socket.bind((local_ip, local_port))
+    print("Server is up and waiting for connections!")
+
+    threads = []
+
+    while True:
+        tcp_socket.listen()
+        (client_connection, (client_ip, client_port)) = tcp_socket.accept()
+        new_client = ClientThread(client_ip, client_port, client_connection)
+        new_client.start()
+        threads.append(new_client)
+
+
+if (__name__ == "__main__"):
+    run()
